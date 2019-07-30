c61167a94aac9aada4f0d2d4167bd610dbdb5d27
kgiari <kgiari17@gmail.com>
2019-07-31 00:22:03 +0300

    Add functioning graph application

---
+++ app/src/main/java/com/example/graphtest/MainActivity.java
@@ -0,0 +1,305 @@
+package com.example.graphtest;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.SingleValueDataSet;
+import com.anychart.charts.CircularGauge;
+import com.anychart.enums.Anchor;
+import com.anychart.graphics.vector.text.HAlign;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static Random r;
+    private static int anxietyLvl, gsr, skt, hr, hrv;
+    private static List<Integer> nums,gsrTotal,hrTotal,hrvTotal,sktTotal;
+    private static Button startBtn, backBtn, btBtn;
+    private static boolean isRunning, isBtOn;
+    private static AnyChartView anyChartView;
+    private static CircularGauge circularGauge;
+    private static double currentValue;
+    private static TextView gsrText, sktText, hrText, hrvText;
+    private static int btDuration, counter;
+    private static Context context;
+    private static String btText;
+    private static Toast btToast;
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initialize();
+        createBtToast();
+        createChart();
+    }
+
+    public void start(View view) {
+
+        //if procedure has not already started and bluetooth is on..
+        if (!isRunning && isBtOn) {
+
+            isRunning = true;
+            startBtn.setText("Stop");
+
+            final Timer t = new Timer();
+            t.schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                    // check if procedure has been stopped
+                    if (!isRunning) {
+                        t.cancel();
+                    }
+
+                    anxietyLvl = r.nextInt(100);
+                    gsr = r.nextInt(100);
+                    skt = r.nextInt(100);
+                    hr = r.nextInt(100);
+                    hrv = r.nextInt(100);
+
+                    nums.add(anxietyLvl);
+                    gsrTotal.add(gsr);
+                    sktTotal.add(skt);
+                    hrTotal.add(hr);
+                    hrvTotal.add(hrv);
+                    counter++;
+
+                    currentValue = anxietyLvl;
+
+                    circularGauge.label(1)
+                            .text("<span style=\"font-size: 20\">" + currentValue + "</span>") //currentValue should be Anxiety Level
+                            .useHtml(true)
+                            .hAlign(HAlign.CENTER);
+
+                    circularGauge.data(new SingleValueDataSet(new Double[]{currentValue}));
+
+                    gsrText.setText("GSR : " + gsr);
+                    sktText.setText("SKT : " + skt);
+                    hrText.setText("HR : " + hr);
+                    hrvText.setText("HRV : " + hrv);
+
+                }
+            }, 0, 1000);
+        }else if(!isRunning && !isBtOn){
+            btToast.show();
+        }else{
+            isRunning = false;
+            startBtn.setText("Start");
+            //ResultsActivity ra = new ResultsActivity(getGsr(),getSkt(),getHr(),getHrv(),getTotalScore());
+            startActivity(new Intent(MainActivity.this, ResultsActivity.class));
+        }
+    }
+
+    public void btCheck(View view){
+
+        if(isBtOn){
+            btBtn.setBackgroundResource(R.drawable.bluetooth_icon_off);
+            isBtOn =false;
+            isRunning = false;
+            startBtn.setText("Start");
+        }else{
+            btBtn.setBackgroundResource(R.drawable.bluetooth_icon);
+            isBtOn =true;
+        }
+
+    }
+
+    public static List<Integer> getTotalScore(){
+        return nums;
+    }
+
+    public static double getGsr(){
+        int sum = 0;
+        for(int i=0;i<gsrTotal.size();i++){
+            sum+= gsrTotal.get(i);
+        }
+        return sum/counter;
+    }
+
+    public static double getSkt(){
+        int sum = 0;
+        for(int i=0;i<sktTotal.size();i++){
+            sum+= sktTotal.get(i);
+        }
+        return sum/counter;
+    }
+
+    public static double getHr(){
+        int sum = 0;
+        for(int i=0;i<hrTotal.size();i++){
+            sum+= hrTotal.get(i);
+        }
+        return sum/counter;
+    }
+
+    public static double getHrv(){
+        int sum = 0;
+        for(int i=0;i<hrvTotal.size();i++){
+            sum+= hrvTotal.get(i);
+        }
+        return sum/counter;
+    }
+
+    private void initialize() {
+
+        isBtOn = false;
+        isRunning = false;
+        r = new Random();
+        counter = 0;
+
+        nums = new ArrayList<Integer>();
+        gsrTotal = new ArrayList<Integer>();
+        hrTotal = new ArrayList<Integer>();
+        hrvTotal = new ArrayList<Integer>();
+        sktTotal = new ArrayList<Integer>();
+
+        startBtn = (Button) findViewById(R.id.startBtn);
+        backBtn = (Button) findViewById(R.id.backBtn);
+        btBtn = (Button) findViewById(R.id.btBtn);
+
+        gsrText = (TextView) findViewById(R.id.gsrTextView);
+        sktText = (TextView) findViewById(R.id.sktTextView);
+        hrText = (TextView) findViewById(R.id.hrTextView);
+        hrvText = (TextView) findViewById(R.id.hrvTextView);
+
+        anyChartView = findViewById(R.id.any_chart_view);
+        circularGauge = AnyChart.circular();
+
+    }
+
+    public void createChart(){
+
+        circularGauge.fill("#fff")
+                .stroke(null)
+                .padding(0, 0, 0, 0)
+                .margin(30, 30, 30, 30);
+        circularGauge.startAngle(0)
+                .sweepAngle(360);
+
+        currentValue = 0;
+
+        circularGauge.data(new SingleValueDataSet(new Double[] { currentValue }));
+
+        circularGauge.axis(0)
+                .startAngle(-150)
+                .radius(80)
+                .sweepAngle(300)
+                .width(3)
+                .ticks("{ type: 'line', length: 4, position: 'outside' }");
+
+        circularGauge.axis(0).labels().position("outside");
+
+        circularGauge.axis(0).scale()
+                .minimum(0)
+                .maximum(100);
+
+        circularGauge.axis(0).scale()
+                .ticks("{interval: 10}")
+                .minorTicks("{interval: 10}");
+
+        circularGauge.needle(0)
+                .stroke(null)
+                .startRadius("6%")
+                .endRadius("38%")
+                .startWidth("2%")
+                .endWidth(0);
+
+        circularGauge.cap()
+                .radius("4%")
+                .enabled(true)
+                .stroke(null);
+
+        circularGauge.label(0)
+                .text("<span style=\"font-size: 25\">Anxiety Level</span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER);
+
+        circularGauge.label(0)
+                .anchor(Anchor.CENTER_TOP)
+                .offsetY(100)
+                .padding(15, 20, 0, 0);
+
+        circularGauge.label(1)
+                .text("<span style=\"font-size: 20\">" + currentValue + "</span>") //currentValue is current Anxiety Level
+                .useHtml(true)
+                .hAlign(HAlign.CENTER);
+
+        circularGauge.label(1)
+                .anchor(Anchor.CENTER_TOP)
+                .offsetY(-100)
+                .padding(5, 10, 0, 0)
+                .background("{fill: 'none', stroke: '#c1c1c1', corners: 3, cornerType: 'ROUND'}");
+
+        circularGauge.range(0,
+                "{\n" +
+                        "    from: 0,\n" +
+                        "    to: 40,\n" +
+                        "    position: 'inside',\n" +
+                        "    fill: 'green 0.5',\n" +
+                        "    stroke: '1 #000',\n" +
+                        "    startSize: 6,\n" +
+                        "    endSize: 6,\n" +
+                        "    radius: 80,\n" +
+                        "    zIndex: 1\n" +
+                        "  }");
+
+        circularGauge.range(1,
+                "{\n" +
+                        "    from: 40,\n" +
+                        "    to: 75,\n" +
+                        "    position: 'inside',\n" +
+                        "    fill: 'yellow 0.5',\n" +
+                        "    stroke: '1 #000',\n" +
+                        "    startSize: 6,\n" +
+                        "    endSize: 6,\n" +
+                        "    radius: 80,\n" +
+                        "    zIndex: 1\n" +
+                        "  }");
+
+        circularGauge.range(2,
+                "{\n" +
+                        "    from: 75,\n" +
+                        "    to: 100,\n" +
+                        "    position: 'inside',\n" +
+                        "    fill: 'red 0.5',\n" +
+                        "    stroke: '1 #000',\n" +
+                        "    startSize: 6,\n" +
+                        "    endSize: 6,\n" +
+                        "    radius: 80,\n" +
+                        "    zIndex: 1\n" +
+                        "  }");
+
+        anyChartView.setChart(circularGauge);
+
+    }
+
+    /*
+    initialize a toast to appear when bluetooth connection is off and start is clicked
+    the toast will only appear after the command btToast.show() is run
+     */
+    public void createBtToast(){
+
+        context = getApplicationContext();
+        btText = "Bluetooth is not connected!";
+        btDuration = Toast.LENGTH_SHORT;
+        btToast = Toast.makeText(context, btText, btDuration);
+        btToast.setGravity(Gravity.CENTER, 0, 0);
+
+    }
+}
---
+++ app/src/main/java/com/example/graphtest/ResultsActivity.java
@@ -0,0 +1,161 @@
+package com.example.graphtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.core.cartesian.series.Line;
+import com.anychart.data.Mapping;
+import com.anychart.data.Set;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.MarkerType;
+import com.anychart.enums.TooltipPositionMode;
+import com.anychart.graphics.vector.Stroke;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ResultsActivity extends AppCompatActivity {
+
+    private static double gsr, skt, hr, hrv;
+    private static List<Integer> anxiety;
+    private static Button backBtn;
+    private static AnyChartView anyChartView;
+    private static TextView gsrText, sktText, hrText, hrvText;
+    private static Cartesian cartesian;
+    private static MainActivity mainData;
+
+//    public ResultsActivity(double gsr, double skt, double hr, double hrv, List<Integer> anxiety){
+//        this.gsr = gsr;
+//        this.skt = skt;
+//        this.hr = hr;
+//        this.hrv = hrv;
+//        this.anxiety = anxiety;
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+        backBtn = (Button) findViewById(R.id.backBtn);
+        gsrText = (TextView) findViewById(R.id.gsrTextView);
+        sktText = (TextView) findViewById(R.id.sktTextView);
+        hrText = (TextView) findViewById(R.id.hrTextView);
+        hrvText = (TextView) findViewById(R.id.hrvTextView);
+
+        mainData = new MainActivity();
+        gsr = mainData.getGsr();
+        skt = mainData.getSkt();
+        hr = mainData.getHr();
+        hrv = mainData.getHrv();
+        anxiety = mainData.getTotalScore();
+
+        gsrText.setText("GSR : "+gsr);
+        sktText.setText("SKT : "+skt);
+        hrText.setText("HR : "+hr);
+        hrvText.setText("HRV : "+hrv);
+
+        anyChartView = findViewById(R.id.any_chart_view);
+        cartesian = AnyChart.line();
+        setGraph();
+    }
+
+    public void setGraph(){
+
+        cartesian.animation(true);
+
+        cartesian.padding(5d, 5d, 5d, 5d);
+
+        cartesian.crosshair().enabled(true);
+        cartesian.crosshair()
+                .yLabel(true)
+                // TODO ystroke
+                .yStroke((Stroke) null, null, null, (String) null, (String) null);
+
+        cartesian.tooltip().positionMode(TooltipPositionMode.POINT);
+
+        cartesian.title("Anxiety Level of the session");
+
+        cartesian.yAxis(0).title("Anxiety Level");
+        cartesian.xAxis(0).title("Time (sec)");
+        cartesian.xAxis(0).labels().padding(5d, 5d, 5d, 5d);
+
+        List<DataEntry> seriesData = new ArrayList<>();
+
+        for(int i=0; i<anxiety.size();i++) {
+            seriesData.add(new CustomDataEntry(""+i, anxiety.get(i)));
+        }
+
+          Set set = Set.instantiate();
+          set.instantiate();
+          set.data(seriesData);
+          Mapping series1Mapping = set.mapAs("{ x: 'x', value: 'value' }");
+//        Mapping series2Mapping = set.mapAs("{ x: 'x', value: 'value2' }");
+//        Mapping series3Mapping = set.mapAs("{ x: 'x', value: 'value3' }");
+
+        Line series1 = cartesian.line(series1Mapping);
+        series1.name("No binaural");
+        series1.hovered().markers().enabled(true);
+        series1.hovered().markers()
+                .type(MarkerType.CIRCLE)
+                .size(4d);
+        series1.tooltip()
+                .position("right")
+                .anchor(Anchor.LEFT_CENTER)
+                .offsetX(5d)
+                .offsetY(5d);
+
+//        Line series2 = cartesian.line(series2Mapping);
+//        series2.name("Whiskey");
+//        series2.hovered().markers().enabled(true);
+//        series2.hovered().markers()
+//                .type(MarkerType.CIRCLE)
+//                .size(4d);
+//        series2.tooltip()
+//                .position("right")
+//                .anchor(Anchor.LEFT_CENTER)
+//                .offsetX(5d)
+//                .offsetY(5d);
+//
+//        Line series3 = cartesian.line(series3Mapping);
+//        series3.name("Tequila");
+//        series3.hovered().markers().enabled(true);
+//        series3.hovered().markers()
+//                .type(MarkerType.CIRCLE)
+//                .size(4d);
+//        series3.tooltip()
+//                .position("right")
+//                .anchor(Anchor.LEFT_CENTER)
+//                .offsetX(5d)
+//                .offsetY(5d);
+
+        cartesian.legend().enabled(true);
+        cartesian.legend().fontSize(13d);
+        cartesian.legend().padding(5d, 5d, 10d, 5d);
+
+        anyChartView.setChart(cartesian);
+    }
+
+
+    public void goBack(View view) {
+        finish();
+    }
+
+    public class CustomDataEntry extends ValueDataEntry {
+
+        CustomDataEntry(String x, Number value) {
+            super(x, value);
+        }
+
+    }
+    }
+
