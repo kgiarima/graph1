ce9fd68dc74503db519280d94c0412e75d2f0ed4
kgiarima <kgiari17@gmail.com>
2019-12-27 15:37:36 +0200

    update Database class to save data

--- app/src/main/java/com/example/graphtest/DatabaseHelper.java
+++ app/src/main/java/com/example/graphtest/DatabaseHelper.java
@@ -7,19 +7,19 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String TABLE_NAME="data_table";
-    private static final String col0="ID";
-    private static final String col1="date";
-    private static final String col2="user";
-    private static final String col3="avg_gsr";
-    private static final String col4="avg_gsr_binaural";
-    private static final String col5="avg_skt";
-    private static final String col6="avg_skt_binaural";
-    private static final String col7="avg_hr";
-    private static final String col8="avg_hr_binaural";
-    private static final String col9="avg_hrv";
-    private static final String col10="avg_hrv_binaural";
-    private static final String col11="avg_anxiety";
-    private static final String col12="avg_anxiety_binaural";
+    private static final String COL0="ID";
+    private static final String COL1="date";
+    private static final String COL2="user";
+    private static final String COL3="avg_gsr";
+    private static final String COL4="avg_gsr_binaural";
+    private static final String COL5="avg_skt";
+    private static final String COL6="avg_skt_binaural";
+    private static final String COL7="avg_hr";
+    private static final String COL8="avg_hr_binaural";
+    private static final String COL9="avg_hrv";
+    private static final String COL10="avg_hrv_binaural";
+    private static final String COL11="avg_anxiety";
+    private static final String COL12="avg_anxiety_binaural";
 
 
     public DatabaseHelper(Context context){
@@ -27,12 +27,20 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        String createTable = "CREATE TABLE "+TABLE_NAME+" (ID INTEGER PRIMARY KEY AUTOINCREMENT, "+COL1+" DATE, "+COL2+" TEXT, "+COL3+ "NUMBER, "+COL4+ "NUMBER, "+COL5+ "NUMBER, "+COL6+ "NUMBER, "+COL7+ "NUMBER, "+COL8+ "NUMBER, "+COL9+ "NUMBER, "+COL10+ "NUMBER, "+COL11+ "NUMBER, "+COL12+ "NUMBER)";
+        db.execSQL(createTable);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
 
+    public boolean addData(String item){
+
+
+        return true;
     }
 }
